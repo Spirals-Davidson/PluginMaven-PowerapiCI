d345a4e57b70f0cc973b5089d1d77180d3a0401f
Deblock Adrien <adrien.deblock@davidson.fr>
2018-06-29 10:36:24 +0200

    Powerapi work on good PID now

--- src/main/java/com/powerapi/PowerapiMojo.java
+++ src/main/java/com/powerapi/PowerapiMojo.java
@@ -91,7 +91,7 @@ public class PowerapiMojo extends AbstractMojo {
     }
 
     private void fillTestAndPowerapiData() {
-        String[] cmd = {"sh", "-c", "(mvn test -DforkCount=0 | grep timestamp= | cut -d '-' -f 2 | tr -d ' ') > test1.csv & echo 'pid: $!'; (powerapi duration 30 modules procfs-cpu-simple monitor --frequency 50 --console --pid $! | grep muid) > data1.csv;"};
+        String[] cmd = {"sh", "-c", "(mvn test -DforkCount=0 | grep timestamp= | cut -d '-' -f 2 | tr -d ' ') > test1.csv & echo 'pid: \\$!'; (powerapi duration 30 modules procfs-cpu-simple monitor --frequency 50 --console --pid \\$! | grep muid) > data1.csv;"};
 
         try {
             Process p = Runtime.getRuntime().exec(cmd);
